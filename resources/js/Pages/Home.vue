d9572956551705fbb726400b29dbd7c34c38c9b4
Pumbaa <133253852+Pumbaa05@users.noreply.github.com>
2024-11-26 21:04:34 +0000

    add: layout and head title

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,19 @@
+<script setup>
+import MainLayout from "@/Layouts/MainLayout.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
 <template>
-    <div></div>
+    <Head title="Home" />
+
+    <MainLayout>
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        Welcome to your Inertia.js app!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
 </template>
-<script></script>
-<style></style>
